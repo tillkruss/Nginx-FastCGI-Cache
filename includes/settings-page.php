17d554bf420b491ef060e1d4362d372efa5c77f9
Chalda Pnuzig <chalda82@gmail.com>
2017-11-22 16:57:43 +0100

    Update settings-page.php

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -16,7 +16,7 @@
 				<th scope="row"><?php _e( 'Cache Zone Path', 'nginx-cache' ); ?></th>
 				<td>
 					<input type="text" class="regular-text code" name="nginx_cache_path" placeholder="/data/nginx/cache" value="<?php echo esc_attr( get_option( 'nginx_cache_path' ) ); ?>" />
-					<p class="description">The absolute path to the location of the cache zone, specified in the Nginx <code>fastcgi_cache_path</code> or <code>proxy_cache_path</code> directive.</p>
+					<p class="description"><?php _e( 'The absolute path to the location of the cache zone, specified in the Nginx <code>fastcgi_cache_path</code> or <code>proxy_cache_path</code> directive.', 'nginx-cache' ) ?></p>
 				</td>
 			</tr>
 			<tr valign="top">
@@ -33,7 +33,7 @@
 		<p class="submit">
 			<?php echo get_submit_button( null, 'primary large', 'submit', false ); ?>
 			&nbsp;
-			<a href="<?php echo wp_nonce_url( admin_url( add_query_arg( 'action', 'purge-cache', $this->admin_page ) ), 'purge-cache' ); ?>" class="button button-secondary button-large delete<?php if ( is_wp_error( $this->is_valid_path() ) ) : ?> disabled<?php endif; ?>"><?php _e( 'Purge Cache', 'nginx' ); ?></a>
+			<a href="<?php echo wp_nonce_url( admin_url( add_query_arg( 'action', 'purge-cache', $this->admin_page ) ), 'purge-cache' ); ?>" class="button button-secondary button-large delete<?php if ( is_wp_error( $this->is_valid_path() ) ) : ?> disabled<?php endif; ?>"><?php _e( 'Purge Cache', 'nginx-cache' ); ?></a>
 		</p>
 
 	</form>
